dc289657588dc2dbe2c3c6b1a96857d114a5b03d
MikhailKolomoitsev <75487015+MikhailKolomoitsev@users.noreply.github.com>
2022-06-22 17:59:33 +0000

    1-09

--- pages/tracks/index.tsx
+++ pages/tracks/index.tsx
@@ -3,9 +3,42 @@ import { Box, Button } from '@mui/material'
 import { useRouter } from 'next/dist/client/router'
 import React from 'react'
 import MainLayout from '../../layouts/MainLayout'
+import { ITrack } from '../../types/track'
 
 const index = () => {
     const router = useRouter()
+    const tracks: ITrack[] = [
+        {
+            _id: '1',
+            name: "track 1",
+            artist: "50 cent",
+            text: "Tesxt",
+            listens: 5,
+            audio: 'http://localhost:5555/audio/',
+            picture: 'http://localhost:5555/audio/',
+            comments: []
+        },
+        {
+            _id: '1',
+            name: "track 1",
+            artist: "50 cent",
+            text: "Tesxt",
+            listens: 5,
+            audio: 'http://localhost:3000/audio/',
+            picture: 'http://localhost:3000/audio/',
+            comments: []
+        },
+        {
+            _id: '1',
+            name: "track 1",
+            artist: "50 cent",
+            text: "Tesxt",
+            listens: 5,
+            audio: 'http://localhost:3000/audio/',
+            picture: 'http://localhost:3000/audio/',
+            comments: []
+        }
+    ]
     return (
         <MainLayout>
             <Grid container justifyContent='center'>
