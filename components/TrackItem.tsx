eee8e9489b602d9401e8034e6271c9a9c8813c25
MikhailKolomoitsev <75487015+MikhailKolomoitsev@users.noreply.github.com>
2022-07-20 22:07:25 +0000

    1-16

--- components/TrackItem.tsx
+++ components/TrackItem.tsx
@@ -1,7 +1,8 @@
-import { Card } from '@mui/material'
+import { Card, Grid, IconButton } from '@mui/material'
 import React from 'react'
 import { ITrack } from '../types/track'
 import styles from '../styles/TrackItem.module.scss'
+import { Delete, Pause, PlayArrow } from '@material-ui/icons'
 
 interface TrackItemProps {
   track: ITrack,
@@ -11,7 +12,21 @@ interface TrackItemProps {
 const TrackItem: React.FC<TrackItemProps> = ({ track, active = false }) => {
   return (
     <Card className={styles.track}>
-      {track.name}
+      <IconButton>
+        {active
+          ? <Pause />
+          : <PlayArrow />
+        }
+      </IconButton>
+      <img width={70} height={70} src={track.picture} />
+      <Grid container direction='column' style={{ width: 200, margin: '0 20px' }}>
+        <div>{track.name}</div>
+        <div style={{ fontSize: 12, color: 'gray' }}>{track.artist}</div>
+      </Grid>
+      {active && <div>02:42 / 03:22</div>}
+      <IconButton style={{marginLeft: 'auto'}}>
+        <Delete />
+      </IconButton>
     </Card>
   )
 }
